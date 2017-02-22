ec810299f9e6096e9adf23a71e017b4b1d228e8f
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2017-02-23 04:01:34 +0800

    Add parallel download test

---
+++ test/parallel-download-test.ts
@@ -0,0 +1,58 @@
+/// <reference path='../node_modules/@types/chai/index.d.ts' />
+
+import fs = require('fs');
+import os = require('os');
+
+import {expect} from 'chai';
+
+import ParallelDownload from '../models/parallel-download';
+
+describe('Parallel download', () => {
+    it('download without saving file', function(done) {
+        this.timeout(5000);
+
+        const fileContentLength: number = 663451;
+        let fileContentLengthCounter: number = 0;
+
+        new ParallelDownload()
+            .start('https://homepages.cae.wisc.edu/~ece533/images/cat.png', 5)
+            .on('data', (data, offset) => {
+                fileContentLengthCounter += data.length;
+            })
+            .on('end', () => {
+                expect(fileContentLengthCounter).to.equal(fileContentLength);
+                done();
+            });
+    });
+
+    it('download and save file', function(done) {
+        this.timeout(5000);
+
+        const fileContentLength: number = 663451;
+        let fileContentLengthCounter: number = 0;
+
+        const directory: string = os.tmpdir();
+
+        new ParallelDownload()
+            .start('https://homepages.cae.wisc.edu/~ece533/images/cat.png', 5, directory)
+            .on('data', (data, offset) => {
+                fileContentLengthCounter += data.length;
+            })
+            .on('end', (filePath) => {
+                expect(fileContentLengthCounter).to.equal(fileContentLength);
+
+                expect(filePath).to.exist;
+
+                fs.lstat(filePath, (err, stats) => {
+                    expect(err).to.be.null;
+                    expect(stats.size).to.equal(fileContentLength);
+
+                    fs.unlink(filePath, (err) => {
+                        expect(err).to.be.null;
+                        
+                        done();
+                    });
+                });
+            });
+    });
+});
