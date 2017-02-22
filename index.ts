dfc52085cf3054676d48ab5308814dd02cd2c53f
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2017-02-23 04:03:03 +0800

    Clean index.js

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
 import ParallelDownload from './models/parallel-download';
 
 export default ParallelDownload;
-
-// new ParallelDownload().start('http://speedtest.ftp.otenet.gr/files/test100k.db', 6,  'C:\\Users\\wiseguy\\Documents\\New folder');
-// new ParallelDownload().start('http://speedtest.ftp.otenet.gr/files/test10Mb.db', 10,  'C:\\Users\\wiseguy\\Documents\\New folder');
-// new ParallelDownload().start('https://zoompf.com/wp-content/uploads/2010/03/HTTP-Range-Request.png', 5,  'C:\\Users\\wiseguy\\Documents\\New folder');
-// new ParallelDownload().start('https://upload.wikimedia.org/wikipedia/commons/b/b8/ESO_Very_Large_Telescope.jpg', 10,  'C:\\Users\\wiseguy\\Documents\\New folder');
-
