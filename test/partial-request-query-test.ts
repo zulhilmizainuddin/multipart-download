a53e96030bc0c23c11eef31cd41a3829c62c983b
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2017-02-23 07:19:56 +0800

    Add test for no Accept-Ranges header

--- test/partial-request-query-test.ts
+++ test/partial-request-query-test.ts
@@ -5,7 +5,7 @@ import {expect} from 'chai';
 import PartialRequestQuery from '../models/partial-request-query';
 
 describe('Partial request query', () => {
-    it('support Accept-Ranges', function(done) {
+    it('with Accept-Ranges header', function(done) {
         this.timeout(5000);
 
         const partialRequestQuery: PartialRequestQuery = new PartialRequestQuery();
@@ -14,6 +14,20 @@ describe('Partial request query', () => {
             .getMetadata('https://homepages.cae.wisc.edu/~ece533/images/cat.png')
             .then((metadata) => {
                 expect(metadata.acceptRanges).to.equal('bytes');
+                expect(metadata.contentLength).to.not.be.NaN;
+                done();
+            });
+    });
+
+    it('without Accept-Ranges header', function(done) {
+        this.timeout(5000);
+
+        const partialRequestQuery: PartialRequestQuery = new PartialRequestQuery();
+
+        partialRequestQuery
+            .getMetadata('https://s-media-cache-ak0.pinimg.com/736x/92/9d/3d/929d3d9f76f406b5ac6020323d2d32dc.jpg')
+            .then((metadata) => {
+                expect(metadata.acceptRanges).to.not.exist;
                 expect(metadata.contentLength).to.not.be.NaN;
                 done();
             });
