6d0afc3fed1f6e82a837a03930cf2a50bfe8640e
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2017-02-25 15:00:07 +0800

    Remove dist directory

--- dist/src/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-const multipart_download_1 = require("./models/multipart-download");
-module.exports = multipart_download_1.default;
--- dist/src/models/accept-ranges.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AcceptRanges {
-}
-AcceptRanges.None = 'none';
-AcceptRanges.Bytes = 'bytes';
-exports.default = AcceptRanges;
