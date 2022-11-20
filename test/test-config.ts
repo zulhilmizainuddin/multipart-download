47e9756f0477c29643c75f62fa24e52c3ccbc100
zulhilmizainuddin <zulhilmi.zainuddin@outlook.com>
2022-11-20 11:29:40 +0800

    update test config

--- test/test-config.ts
+++ test/test-config.ts
@@ -4,7 +4,7 @@ export interface ContentLengthUrlPair {
 }
 
 export class TestConfig {
-    public static readonly AcceptRangesSupportedUrl: ContentLengthUrlPair = {contentLength: 663451, url: 'https://homepages.cae.wisc.edu/~ece533/images/cat.png'};
+    public static readonly AcceptRangesSupportedUrl: ContentLengthUrlPair = {contentLength: 992514, url: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg'};
     public static readonly AcceptRangesUnsupportedUrl: ContentLengthUrlPair = {contentLength: 51567, url: 'https://s-media-cache-ak0.pinimg.com/736x/92/9d/3d/929d3d9f76f406b5ac6020323d2d32dc.jpg'};
 
     public static readonly Timeout: number = 10000;
